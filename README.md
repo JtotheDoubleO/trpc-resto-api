5fa16958ea1c98f3bb85a174bebebd8c966a628d
JtotheDoubleO <joo291998@gmail.com>
2024-06-28 23:41:47 +0700

    feat(readme): modify readme to add detail on setting up and running locally

--- README.md
+++ README.md
@@ -14,36 +14,33 @@ We might aim for backend and frontend repositories separation of:
 
 ... in that case checkout this boilerplate.
 
-## Running
+## Running locally
 
 _Easily set up a local development environment_
 
-- fork & clone repo
+- clone repo
 - `npm install`
+- `npm run dev`
 - make changes to tRPC API & push - new [package](https://www.npmjs.com/package/trpc-api-boilerplate) is released 📦 [![npm version][npm-badge]][npm-url]
-- install newly released package `npm install trpc-api-boilerplate` in any frontend app 🚀
-
-## Example Repo
-
-Example frontend app repositories:
-
-- [tRPC Frontend Boilerplate](https://github.com/mkosir/trpc-fe-boilerplate-vite) - Vite
-- [tRPC Frontend Boilerplate](https://github.com/mkosir/trpc-fe-boilerplate-next) - Nextjs
-
-### Avoid publishing package?
-
-If for whatever reason publishing a package is not an option:
-
-- privacy concerns
-- faster development iterations - skip CI
-- ...
 
 Use repository to share types by running `npm run trpc-api-export` and push code changes.  
 In your [frontend app](https://github.com/mkosir/trpc-fe-boilerplate-vite/blob/main/package.json#L7) consume types by running `npm run trpc-api-import`.
 
-<!-- Badges -->
+## Database Setup
 
-[ci-badge]: https://github.com/mkosir/trpc-api-boilerplate/actions/workflows/CI.yml/badge.svg
-[ci-url]: https://github.com/mkosir/trpc-api-boilerplate/actions/workflows/CI.yml
-[npm-url]: https://www.npmjs.com/package/trpc-api-boilerplate
-[npm-badge]: https://img.shields.io/npm/v/trpc-api-boilerplate.svg
+Before this step, copy the env.example file content into .env file and make changes to these environment variables
+Set up Docker container for PostgresSQL database and PgAdmin for monitoring database
+
+```shell
+npm run ecosystem:up
+```
+
+## Database migrating and seeding
+
+Run these command to generate database, migrate schema and seed mock data
+
+```shell
+npm run db:generate
+npm run db:migrate
+npm run db:seed
+```
--- trpc-api-export/dist/index.d.ts
+++ trpc-api-export/dist/index.d.ts
@@ -1,9 +1,9 @@
-import * as _prisma_client from '.prisma/client';
 import * as superjson from 'superjson';
 import * as _trpc_server from '@trpc/server';
 import * as express from 'express';
 import * as qs from 'qs';
 import * as express_serve_static_core from 'express-serve-static-core';
+import { Prisma } from '@prisma/client';
 
 declare const appRouter: _trpc_server.CreateRouterInner<
   _trpc_server.RootConfig<{
@@ -16,171 +16,48 @@ declare const appRouter: _trpc_server.CreateRouterInner<
     transformer: typeof superjson.default;
   }>,
   {
-    resto: _trpc_server.CreateRouterInner<
-      _trpc_server.RootConfig<{
-        ctx: {
+    getRestaurants: _trpc_server.BuildProcedure<
+      'query',
+      {
+        _config: _trpc_server.RootConfig<{
+          ctx: {
+            req: express.Request<
+              express_serve_static_core.ParamsDictionary,
+              any,
+              any,
+              qs.ParsedQs,
+              Record<string, any>
+            >;
+            res: express.Response<any, Record<string, any>>;
+          };
+          meta: object;
+          errorShape: _trpc_server.DefaultErrorShape;
+          transformer: typeof superjson.default;
+        }>;
+        _meta: object;
+        _ctx_out: {
           req: express.Request<express_serve_static_core.ParamsDictionary, any, any, qs.ParsedQs, Record<string, any>>;
           res: express.Response<any, Record<string, any>>;
         };
-        meta: object;
-        errorShape: _trpc_server.DefaultErrorShape;
-        transformer: typeof superjson.default;
-      }>,
-      {
-        list: _trpc_server.BuildProcedure<
-          'query',
-          {
-            _config: _trpc_server.RootConfig<{
-              ctx: {
-                req: express.Request<
-                  express_serve_static_core.ParamsDictionary,
-                  any,
-                  any,
-                  qs.ParsedQs,
-                  Record<string, any>
-                >;
-                res: express.Response<any, Record<string, any>>;
-              };
-              meta: object;
-              errorShape: _trpc_server.DefaultErrorShape;
-              transformer: typeof superjson.default;
-            }>;
-            _meta: object;
-            _ctx_out: {
-              req: express.Request<
-                express_serve_static_core.ParamsDictionary,
-                any,
-                any,
-                qs.ParsedQs,
-                Record<string, any>
-              >;
-              res: express.Response<any, Record<string, any>>;
-            };
-            _input_in:
-              | {
-                  category?: string | null | undefined;
-                  q?: string | null | undefined;
-                }
-              | null
-              | undefined;
-            _input_out:
-              | {
-                  category?: string | null | undefined;
-                  q?: string | null | undefined;
-                }
-              | null
-              | undefined;
-            _output_in: typeof _trpc_server.unsetMarker;
-            _output_out: typeof _trpc_server.unsetMarker;
-          },
-          (
-            | {
-                id: string;
-                createdAt: Date;
-                updatedAt: Date;
-                name: string;
-                images: string[];
-                featured: PrismaJson.Featured | null;
-                rating: number;
-                ratingCount: bigint;
-                city: string;
-                category: string;
-                priceRange: string;
-                desc: string;
-                isFavorite: boolean;
-              }
-            | {
-                featured: _prisma_client.Prisma.JsonObject;
-                id: string;
-                createdAt: Date;
-                updatedAt: Date;
-                name: string;
-                images: string[];
-                rating: number;
-                ratingCount: bigint;
-                city: string;
-                category: string;
-                priceRange: string;
-                desc: string;
-                isFavorite: boolean;
-              }
-          )[]
-        >;
-        update: _trpc_server.BuildProcedure<
-          'mutation',
-          {
-            _config: _trpc_server.RootConfig<{
-              ctx: {
-                req: express.Request<
-                  express_serve_static_core.ParamsDictionary,
-                  any,
-                  any,
-                  qs.ParsedQs,
-                  Record<string, any>
-                >;
-                res: express.Response<any, Record<string, any>>;
-              };
-              meta: object;
-              errorShape: _trpc_server.DefaultErrorShape;
-              transformer: typeof superjson.default;
-            }>;
-            _meta: object;
-            _ctx_out: {
-              req: express.Request<
-                express_serve_static_core.ParamsDictionary,
-                any,
-                any,
-                qs.ParsedQs,
-                Record<string, any>
-              >;
-              res: express.Response<any, Record<string, any>>;
-            };
-            _input_in: {
-              id: string;
-              patch: {
-                id?: string | undefined;
-                isFavorite?: boolean | undefined;
-                images?: string[] | undefined;
-                featured?:
-                  | {
-                      text: string;
-                      icon: string;
-                    }
-                  | undefined;
-                name?: string | undefined;
-                rating?: number | undefined;
-                ratingCount?: number | undefined;
-                city?: string | undefined;
-                category?: string | undefined;
-                priceRange?: string | undefined;
-                desc?: string | undefined;
-              };
-            };
-            _input_out: {
-              id: string;
-              patch: {
-                id?: string | undefined;
-                isFavorite?: boolean | undefined;
-                images?: string[] | undefined;
-                featured?:
-                  | {
-                      text: string;
-                      icon: string;
-                    }
-                  | undefined;
-                name?: string | undefined;
-                rating?: number | undefined;
-                ratingCount?: number | undefined;
-                city?: string | undefined;
-                category?: string | undefined;
-                priceRange?: string | undefined;
-                desc?: string | undefined;
-              };
-            };
-            _output_in: typeof _trpc_server.unsetMarker;
-            _output_out: typeof _trpc_server.unsetMarker;
-          },
-          {
+        _input_in:
+          | {
+              category?: string | null | undefined;
+              q?: string | null | undefined;
+            }
+          | null
+          | undefined;
+        _input_out:
+          | {
+              category?: string | null | undefined;
+              q?: string | null | undefined;
+            }
+          | null
+          | undefined;
+        _output_in: typeof _trpc_server.unsetMarker;
+        _output_out: typeof _trpc_server.unsetMarker;
+      },
+      (
+        | {
             id: string;
             createdAt: Date;
             updatedAt: Date;
@@ -195,56 +72,105 @@ declare const appRouter: _trpc_server.CreateRouterInner<
             desc: string;
             isFavorite: boolean;
           }
-        >;
-      }
+        | {
+            featured: Prisma.JsonObject;
+            id: string;
+            createdAt: Date;
+            updatedAt: Date;
+            name: string;
+            images: string[];
+            rating: number;
+            ratingCount: bigint;
+            city: string;
+            category: string;
+            priceRange: string;
+            desc: string;
+            isFavorite: boolean;
+          }
+      )[]
     >;
-    util: _trpc_server.CreateRouterInner<
-      _trpc_server.RootConfig<{
-        ctx: {
+    addFavorite: _trpc_server.BuildProcedure<
+      'mutation',
+      {
+        _config: _trpc_server.RootConfig<{
+          ctx: {
+            req: express.Request<
+              express_serve_static_core.ParamsDictionary,
+              any,
+              any,
+              qs.ParsedQs,
+              Record<string, any>
+            >;
+            res: express.Response<any, Record<string, any>>;
+          };
+          meta: object;
+          errorShape: _trpc_server.DefaultErrorShape;
+          transformer: typeof superjson.default;
+        }>;
+        _meta: object;
+        _ctx_out: {
           req: express.Request<express_serve_static_core.ParamsDictionary, any, any, qs.ParsedQs, Record<string, any>>;
           res: express.Response<any, Record<string, any>>;
         };
-        meta: object;
-        errorShape: _trpc_server.DefaultErrorShape;
-        transformer: typeof superjson.default;
-      }>,
+        _input_in: {
+          id: string;
+          patch: {
+            id?: string | undefined;
+            isFavorite?: boolean | undefined;
+            images?: string[] | undefined;
+            featured?:
+              | {
+                  text: string;
+                  icon: string;
+                }
+              | undefined;
+            name?: string | undefined;
+            rating?: number | undefined;
+            ratingCount?: number | undefined;
+            city?: string | undefined;
+            category?: string | undefined;
+            priceRange?: string | undefined;
+            desc?: string | undefined;
+          };
+        };
+        _input_out: {
+          id: string;
+          patch: {
+            id?: string | undefined;
+            isFavorite?: boolean | undefined;
+            images?: string[] | undefined;
+            featured?:
+              | {
+                  text: string;
+                  icon: string;
+                }
+              | undefined;
+            name?: string | undefined;
+            rating?: number | undefined;
+            ratingCount?: number | undefined;
+            city?: string | undefined;
+            category?: string | undefined;
+            priceRange?: string | undefined;
+            desc?: string | undefined;
+          };
+        };
+        _output_in: typeof _trpc_server.unsetMarker;
+        _output_out: typeof _trpc_server.unsetMarker;
+      },
       {
-        seedDb: _trpc_server.BuildProcedure<
-          'mutation',
-          {
-            _config: _trpc_server.RootConfig<{
-              ctx: {
-                req: express.Request<
-                  express_serve_static_core.ParamsDictionary,
-                  any,
-                  any,
-                  qs.ParsedQs,
-                  Record<string, any>
-                >;
-                res: express.Response<any, Record<string, any>>;
-              };
-              meta: object;
-              errorShape: _trpc_server.DefaultErrorShape;
-              transformer: typeof superjson.default;
-            }>;
-            _ctx_out: {
-              req: express.Request<
-                express_serve_static_core.ParamsDictionary,
-                any,
-                any,
-                qs.ParsedQs,
-                Record<string, any>
-              >;
-              res: express.Response<any, Record<string, any>>;
-            };
-            _input_in: typeof _trpc_server.unsetMarker;
-            _input_out: typeof _trpc_server.unsetMarker;
-            _output_in: typeof _trpc_server.unsetMarker;
-            _output_out: typeof _trpc_server.unsetMarker;
-            _meta: object;
-          },
-          'Database initialized successfully.'
-        >;
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        name: string;
+        images: string[];
+        featured: PrismaJson.Featured | null;
+        rating: number;
+        ratingCount: bigint;
+        city: string;
+        category: string;
+        priceRange: string;
+        desc: string;
+        isFavorite: boolean;
       }
     >;
   }
