a40ea16edc5870df95d0b111795b784af55fdccc
JtotheDoubleO <joo291998@gmail.com>
2024-06-28 22:49:04 +0700

    fix(resto): fix resto schema

--- src/db/instance.ts
+++
@@ -1,7 +0,0 @@
-import { DeepMutable, mockRestos } from 'utils';
-
-import { Restos } from './Resto';
-
-export const db = {
-  restos: structuredClone(mockRestos) as unknown as DeepMutable<Restos>,
-};
